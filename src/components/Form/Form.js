22c5cf8f2d154aa21a8c00b31bf7636d8251c696
David Anitoiu <david.anitoiu@gmail.com>
2020-10-24 07:21:34 +0200

    Adjusted basic styling

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import classes from './Form.module.css';
 
 function Form() {
     return (
         <form name="verify">
-            <div className="inputs">
+            <div className={classes.inputs}>
                 <input type="text" name="n1" maxLength="1" />
                 <input type="text" name="n2" maxLength="1" />
                 <input type="text" name="n3" maxLength="1" />
