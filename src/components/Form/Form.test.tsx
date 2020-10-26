02519a0fab8c5b45d1f288e33573408c96fcb708
David Anitoiu <david.anitoiu@gmail.com>
2020-10-26 05:26:48 +0100

    Added tests for basic functionality
    * moved the accidentally created Form.test.js to tsx

---
+++ src/components/Form/Form.test.tsx
@@ -0,0 +1,42 @@
+import { fireEvent, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import Form from '.';
+
+const setup = () => {
+    const { container } = render(<Form />);
+  
+    const inputs = container.querySelectorAll('input');
+    const form = container.querySelector('form');
+    const submit = container.querySelector('button');
+  
+    return { inputs, form, submit }
+  }
+
+describe('Form', () => {
+    it('should have 6 inputs & a button by default', () => {
+        const {inputs, submit} = setup();
+    
+        expect(inputs.length).toBe(6);
+        expect(submit).toBeInTheDocument();
+    });
+
+    it('should correctly accept paste events', () => {
+        const {form} = setup();
+
+        fireEvent.paste(form!,{
+            clipboardData: {
+                getData: () => 'string'
+            }
+        })
+
+        expect(form).toHaveFormValues({
+            n1: 's',
+            n2: 't',
+            n3: 'r',
+            n4: 'i',
+            n5: 'n',
+            n6: 'g'
+        })
+    });
+})
